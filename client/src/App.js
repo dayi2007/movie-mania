0e92cb7199cd16390168b25c32f6640edcb4c421
Dayanna R <dayi2007@hotmail.com>
2021-10-25 11:21:25 -0400

    pictures

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link, Route } from "react-router-dom";
 
 const API_URL= "https://imdb-api.com/en/API/SearchMovie"
 // const API_URL= "https://imdb-api.com/en/API/Search"
@@ -8,14 +9,16 @@ const API_KEY= "k_uqk2e0we"
 
 function App() {
 
-  const[toggleFetch, setToggleFetch] =useState(true);
+  const [toggleFetch, setToggleFetch] = useState(true);
+  const [movieSearch, setMovieSearch] = useState([]);
+
 
   useEffect (() => {
     console.log("Getting Started");  
     const getMovies = async () => {
     const resp = await axios.get(`${API_URL}/${API_KEY}/inception`)
     console.log(resp.data.results);
-    // setBlogPosts(resp.data.records)
+    setMovieSearch(resp.data.results);
   }
   getMovies();
 }, [toggleFetch]);
@@ -23,6 +26,16 @@ function App() {
 
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+        {movieSearch.map((movie) =>( 
+          <div>
+         <h1 key={movie.id}>{movie.title}</h1> 
+         <img src={movie.image} alt={movie.title}/>
+         </div>
+        ))} 
+
      
     </div>
   );
